3361f2d793bb61c72812730d6cd102c6695d4419
MohitSingh65 <mohitsinghpanwar65@gmail.com>
2024-08-22 23:33:56 +0530

    Fixes

--- practice_programs/conference_tickets.go
+++ practice_programs/conference_tickets.go
@@ -1,18 +1,19 @@
 package main
 
-import "fmt"
+import ("fmt" 
+		"strings")
 
 func main() {
 	var conferenceName = "Go Conference"
 	var remainingTickets uint = 50
+	var userTickets uint
+	var firstName string
+	var lastName string
+	var email string
+	var bookings []string
 	fmt.Printf("Welcome to %v Ticketing System\n", conferenceName)
 	fmt.Print("Get your conference tickets here\n")
 	for {
-		var userTickets uint
-		var firstName string
-		var lastName string
-		var email string
-		var bookings []string
 		fmt.Println("Enter your first name:")
 		fmt.Scan(&firstName)
 
@@ -22,16 +23,40 @@ func main() {
 		fmt.Println("Enter your email:")
 		fmt.Scan(&email)
 
-		fmt.Println("Enter number of tickets:")
-		fmt.Scan(&userTickets)
+		enterTickets:
+			fmt.Println("Enter number of tickets:")
+			fmt.Scan(&userTickets)
+		
+
+		if userTickets <= 0 {
+			fmt.Println("Please enter a value more than 0.")
+			goto enterTickets
+		}
+
+		if userTickets > remainingTickets{
+			fmt.Printf("We only have %v tickets remaining. You cannot book more than %v tickets\n.", remainingTickets, remainingTickets)
+			goto enterTickets
+		}
+		
 
 		remainingTickets = remainingTickets - userTickets
 		bookings = append(bookings, firstName + " " + lastName)
 
-		fmt.Printf("These are all our bookings: %v\n", bookings)
-
 		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		
+		firstNames := []string{}
+		for _, booking := range bookings{
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("These are all our bookings: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			fmt.Println("The conference is booked out. Please visit next year. :)")
+			break
+		}
+
 
 	}
 }
