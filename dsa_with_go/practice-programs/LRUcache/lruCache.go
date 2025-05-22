888f9ad5be215fb5793d89c90e4c38f452fc7d01
MohitSingh65 <mohitsinghpanwar65@gmail.com>
2025-05-22 14:11:03 +0530

    Add new programs

--- dsa_with_go/practice-programs/LRUcache/lruCache.go
+++ dsa_with_go/practice-programs/LRUcache/lruCache.go
@@ -1,21 +1,41 @@
 package main
 
-import "fmt"
+type Node[V any] struct {
+	value V
+	next  *Node[V]
+	prev  *Node[V]
+}
 
-type Node struct {
-	value int
-	next  *Node
-	prev  *Node
+func createNode[V any](value V) *Node[V] {
+	return &Node[V]{value: value}
 }
 
 type LRU[K comparable, V any] struct {
-	length int
+	length        int
+	head          *Node[V]
+	tail          *Node[V]
+	lookup        map[K]*Node[V]
+	reverseLookup map[*Node[V]]*K
+}
+
+func NewLRU[K comparable, V any](capacity int) *LRU[K, V] {
+	return &LRU[K, V]{
+		length:        0,
+		head:          nil,
+		tail:          nil,
+		lookup:        make(map[K]*Node[V]),
+		reverseLookup: make(map[*Node[V]]*K),
+	}
 }
 
 func (c *LRU[K, V]) update(key K, value V) {
-	fmt.Print("Hello")
 }
 
-func main() {
-	fmt.Println("Hello, world!")
+func (c *LRU[K, V]) get(key K) (V, bool) {
+	node, ok := c.lookup[key]
+	if !ok {
+		var zero V
+		return zero, false
+	}
+	return node.value, true
 }
